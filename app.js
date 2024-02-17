cae6096511c7e403e9310c28fbf0a32880dd7981
Daniel Saavedra <cesardsaavedra@gmail.com>
2024-02-17 15:56:13 -0300

    Micro desafío paso 2 completo

--- app.js
+++ app.js
@@ -6,9 +6,8 @@ const app = express();
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-// Resto de tu código para configurar rutas, middleware, y escuchar en un puerto
 
-const PORT = 3030; // Puedes cambiar el número de puerto según tus preferencias
+const PORT = 3030; 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
